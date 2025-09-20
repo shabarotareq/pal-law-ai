61c5fcf24f952f0bb35bdc4c9b052fdab035af46
tareq shabaro <shabarotareq@gmail.com>
2025-09-20 10:55:57 +0300

    Add files via upload

---
+++ frontend/src/constants.js
@@ -0,0 +1,39 @@
+// مصادر قانونية موثوقة ذات أولوية
+export const OFFICIAL_SOURCES = [
+  "official-gazette",
+  "ministry",
+  "court",
+  "gov",
+  "palestine-laws",
+  "muqtafi",
+  "wafa.ps",
+  "aman.ps",
+];
+
+// نطاقات المواقع الرسمية الفلسطينية
+export const PALESTINIAN_OFFICIAL_DOMAINS = [
+  "*.gov.ps",
+  "*.palestine.gov.ps",
+  "*.wafa.ps",
+  "*.aman.ps",
+  "*.pcbs.gov.ps",
+];
+
+// أنواع الوثائق القانونية
+export const LEGAL_DOCUMENT_TYPES = {
+  LAW: "law",
+  DECREE: "decree",
+  REGULATION: "regulation",
+  COURT_DECISION: "court_decision",
+  LEGAL_OPINION: "legal_opinion",
+};
+
+// أقسام القانون
+export const LEGAL_CATEGORIES = {
+  CIVIL: "civil_law",
+  CRIMINAL: "criminal_law",
+  COMMERCIAL: "commercial_law",
+  LABOR: "labor_law",
+  ADMINISTRATIVE: "administrative_law",
+  CONSTITUTIONAL: "constitutional_law",
+};
