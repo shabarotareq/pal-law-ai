4cde3c4e300358ad1c6938c431fcecf3f462a0cd
tareq shabaro <shabarotareq@gmail.com>
2025-09-20 10:46:51 +0300

    Add files via upload

---
+++ frontend/src/pages/CivilLaw.jsx
@@ -0,0 +1,13 @@
+// pages/laws/CivilLaw.jsx
+import React from "react";
+
+const CivilLaw = () => {
+  return (
+    <div>
+      <h1>القانون المدني</h1>
+      <p>محتوى الصفحة...</p>
+    </div>
+  );
+};
+
+export default CivilLaw;
