aa5cd48b5ec4d0c388c01ad3aafd3bddce17c255
tareq shabaro <shabarotareq@gmail.com>
2025-09-21 19:29:53 +0300

    Update _app.js

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import "../app/globals.css"; // ✅ الاستدعاء الصحيح للـ CSS
+import "/globals.css"; // ✅ الاستدعاء الصحيح للـ CSS
 
 import { useState, useEffect } from "react";
 import { LegalKnowledgeProvider } from "../contexts/LegalKnowledgeContext";
@@ -61,3 +61,4 @@ function MyApp({ Component, pageProps }) {
 }
 
 export default MyApp;
+
